9f3998d0884a769f6e75acf0b24d9650b8a05a8a
pswoo0323 <psw8554@gmail.com>
2025-05-09 10:54:02 +0900

    gtest 추가

---
+++ build/CMakeFiles/sonar_scanner_example.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sonar_scanner_example.dir/src/main.cpp.o"
+  "CMakeFiles/sonar_scanner_example.dir/src/main.cpp.o.d"
+  "sonar_scanner_example"
+  "sonar_scanner_example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sonar_scanner_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
